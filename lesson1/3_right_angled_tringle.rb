ef51f5a45a3ae40096d1c071f6a7e2edd0fafee6
Anastasiya <nastylia@gmail.com>
2016-08-16 12:12:26 +0300

    for ex#3 - now checking all triangles to be isosceles

--- lesson1/3_right_angled_tringle.rb
+++ lesson1/3_right_angled_tringle.rb
@@ -8,11 +8,12 @@ sides[2] = gets.chomp.to_f
 sides.sort!
 if sides[0]**2 + sides[1]**2 == sides[2]**2
 	puts "Треугольник прямоугольный"
-	if sides[0] == sides[1]
-		puts "А также треугольник равнобедренный"
-	end
 else
 	puts "Треугольник не прямоугольный"
+end
+
+if sides[0] == sides[1]
+	puts "А также треугольник равнобедренный"
 end
 
 
